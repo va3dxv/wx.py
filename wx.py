7caaf5e664422ab794c8e5e8ad7df4abc5aa7ad7
va3dxv <va3dxv@CALLOUS.disregard.net>
2019-01-02 02:19:02 -0500

    built in call to voicerss added vars

--- wx.py
+++ wx.py
@@ -21,37 +21,63 @@
 # 5 */2 * * * /usr/local/sbin/wx.py -f >/dev/null 2>&1
 #
 # add to /etc/asterisk/rpt.conf under [functions]
-# 86=cmd,asterisk -rx "rpt localplay 47960 /etc/asterisk/custom/wxcurrent"
-# 87=cmd,asterisk -rx "rpt localplay 47960 /etc/asterisk/custom/wxforecast"
-#####################################################################################################
+# 86=cmd,asterisk -rx "rpt localplay 99999 /etc/asterisk/custom/wxcurrent"
+# 87=cmd,asterisk -rx "rpt localplay 99999 /etc/asterisk/custom/wxforecast"
+#####################################
 import argparse
-import json
 import requests
 import xmltodict
 import subprocess
 import shlex
-import sys
+#
+# configuration
+#
+localxmlfeed="http://dd.weather.gc.ca/citypage_weather/xml/ON/s0000070_e.xml"
+#
+# LISTING of XML feeds and their cities, here:
+# http://dd.weather.gc.ca/citypage_weather/xml/siteList.xml
+# Corresponds to ACTUAL XML feeds here:
+# http://dd.weather.gc.ca/citypage_weather/xml/ON (replace ON with your province)
+#
+# set your voicerss API key here
+voicersskey = "yourvoicerssapikeygoeshere"
+# set your desired voice language here
+voicersslang = "en-us"
+# set speed of speech here
+voicerssspeed = "-1"
+# set format of initial audio before converting to ulaw
+voicerssformat = "44khz_16bit_mono"
+#
+#
+# end configuration
+#
+temppath = "/tmp/"
+aslpath = "/etc/asterisk/custom/"
+scriptname = "wx"
+aslfile = aslpath + "wx"
+ffiletxt = temppath + scriptname + "forecast.txt"
+ffilemp3 = temppath + scriptname + "forecast.mp3"
+ffilewav = temppath + scriptname + "forecast.wav"
+ffileul = aslfile + "forecast.ul"
+cfiletxt = temppath + scriptname + "current.txt"
+cfilemp3 = temppath + scriptname + "current.mp3"
+cfilewav = temppath + scriptname + "current.wav"
+cfileul = aslfile + "current.ul"
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-f", help="Record forecast and TTS it.", action = "store_true", default=False)
-parser.add_argument("-c", help="Record current conditions and TTS it.", action = "store_true", default=True)
+parser.add_argument("-f", help="Grab forecast and convert to speech.", action = "store_true", default=False)
+parser.add_argument("-c", help="Grab current conditions and convert to speech.", action = "store_true", default=True)
 
 args = parser.parse_args()
 
 xml_data = requests.get(
-    url="http://dd.weather.gc.ca/citypage_weather/xml/ON/s0000070_e.xml"
+    url=localxmlfeed
 )
-
-# LISTING of XML feeds and their cities, here:
-# http://dd.weather.gc.ca/citypage_weather/xml/siteList.xml
-# Corresponds to ACTUAL XML feeds here:
-# http://dd.weather.gc.ca/citypage_weather/xml/ON/
-
 weather_data = xmltodict.parse(xml_data.text)
-
+# forecast weather
 if args.f:
     print ("tts forecast wx")
-    file = open("/tmp/wxforecast.txt", "w")
+    file = open(ffiletxt, "w")
     file.write("Forecast for Eastern Ontario...\r\n")
     for forecast in weather_data["siteData"]["forecastGroup"]["forecast"][0:4]:
             file.write("%s, %s. \r\n" % (
@@ -61,15 +87,27 @@ if args.f:
     file.write("End of forecast.\r\n")
     file.close()
 
-    subprocess.call(shlex.split("/usr/local/sbin/tts_audio.sh /tmp/wxforecast.txt"))
-    subprocess.call(shlex.split("rm -f /tmp/wxforecast.txt"))
-    subprocess.call(shlex.split("rm -f /etc/asterisk/custom/wxforecast.ul"))
-    subprocess.call(shlex.split("mv /tmp/wxforecast.ul /etc/asterisk/custom"))
-
+    bandreport = open(ffiletxt, "r")
+    getmp3 = requests.get("http://api.voicerss.org/",
+                          data={"key": voicersskey, "r": voicerssspeed,
+                                "src": bandreport, "hl": voicersslang, "f": voicerssformat}
+                          )
+    bandreport.close()
+    mp3file = open(ffilemp3, "wb")
+    mp3file.write(getmp3.content)
+    mp3file.close()
+# convert to wav with lame (apt-get install lame) then to ulaw with sox (apt-get install sox)
+    subprocess.call(shlex.split("lame --decode " + ffilemp3 + " " + ffilewav))
+    subprocess.call(shlex.split("sox -V " + ffilewav + " -r 8000 -c 1 -t ul " + ffileul))
+# cleanup
+    subprocess.call(shlex.split("rm -f " + ffiletxt))
+    subprocess.call(shlex.split("rm -f " + ffilemp3))
+    subprocess.call(shlex.split("rm -f " + ffilewav))
+# current weather
 elif args.c:
     print ("tts current wx")
-    file = open("/tmp/wxcurrent.txt", "w")
-    file.write("The temperature is currently %s with a windchill of %s and %s%s humidity. Current windspeed %s%s. Barometric pressure %s %s and %s. %s.\r\n" % \
+    file = open(cfiletxt, "w")
+    file.write("The temperature is currently %s with a windchill of %s and %s%s humidity. Current windspeed %s%s. Barometric pressure %s %s and %s..\r\n" % \
                (
                    weather_data["siteData"]["currentConditions"]["temperature"]["#text"],
                    weather_data["siteData"]["currentConditions"]["windChill"]["#text"],
@@ -80,13 +118,23 @@ elif args.c:
                    weather_data["siteData"]["currentConditions"]["pressure"]["#text"],
                    weather_data["siteData"]["currentConditions"]["pressure"]["@units"],
                    weather_data["siteData"]["currentConditions"]["pressure"]["@tendency"],
-                   weather_data["siteData"]["warnings"]["event"]["@description"],
                )
                )
 
     file.close()
-
-    subprocess.call(shlex.split("/usr/local/sbin/tts_audio.sh /tmp/wxcurrent.txt"))
-    subprocess.call(shlex.split("rm -f /tmp/wxcurrent.txt"))
-    subprocess.call(shlex.split("rm -f /etc/asterisk/custom/wxcurrent.ul"))
-    subprocess.call(shlex.split("mv /tmp/wxcurrent.ul /etc/asterisk/custom"))
+    bandreport = open(cfiletxt, "r")
+    getmp3 = requests.get("http://api.voicerss.org/",
+                          data={"key": voicersskey, "r": voicerssspeed,
+                                "src": bandreport, "hl": voicersslang, "f": voicerssformat}
+                          )
+    bandreport.close()
+    mp3file = open(cfilemp3, "wb")
+    mp3file.write(getmp3.content)
+    mp3file.close()
+# convert to wav with lame (apt-get install lame) then to ulaw with sox (apt-get install sox)
+    subprocess.call(shlex.split("lame --decode " + cfilemp3 + " " + cfilewav))
+    subprocess.call(shlex.split("sox -V " + cfilewav + " -r 8000 -c 1 -t ul " + cfileul))
+# cleanup
+    subprocess.call(shlex.split("rm -f " + cfiletxt))
+    subprocess.call(shlex.split("rm -f " + cfilemp3))
+    subprocess.call(shlex.split("rm -f " + cfilewav))
